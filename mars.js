ae6399f2c00cf8f4fedb344b7cbaf35d3a715ada
crazy123456kp <78312176+crazy123456kp@users.noreply.github.com>
2021-01-31 19:42:50 +0300

    Update mars.js

--- mars.js
+++ mars.js
@@ -519,7 +519,7 @@ const MARS498 = new Discord.Client();
 const MARS499 = new Discord.Client();
 const MARS500 = new Discord.Client();
 /// by mars
-MARS.login("");
+MARS.login("NzkwMjUyODg4NDAwMTk5Njkw.X996hw.-ArW8vW4PtOxhruTG0-DMX3xaU");
 MARS2.login("");
 MARS3.login("");
 MARS4.login("");
